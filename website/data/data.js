2a4315f8556e7d4551cf82172f5e02990183a5d2
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2010-05-02 15:31:00 +1200

    Cleanup src tree

--- website/data/data.js
+++
@@ -1,4 +0,0 @@
-{
-    released: "0.7.0",
-    developing: "0.7.1"     
-}
--- website/library/v0.7/assets/examples/planets/jupiter/jupiter.js
+++
@@ -1,9 +0,0 @@
-SceneJS.renderer({ enableTexture2D: true },
-                SceneJS.texture({
-			layers: [{
-		        	uri:"http://scenejs.org/library/textures/planets/jupiter/jupitermap.jpg"
-			}]        		
-                },
-                        SceneJS.objects.sphere({ rings: 30, slices: 30})
-                        )                
-        )
