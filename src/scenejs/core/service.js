97c0ccea3e1141e6a9bf19edf0528c435d90df93
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2010-10-01 13:06:32 +1300

    Fixed syntax bug in SceneJS.Services

--- src/scenejs/core/service.js
+++ src/scenejs/core/service.js
@@ -1,21 +1,12 @@
 /**
  * SceneJS IOC service container
  */
+
 SceneJS.Services = new (function() {
 
-    /** ID of node loader service
-     */
     this.NODE_LOADER_SERVICE_ID = "node-loader";
 
-    this._services = {
-
-        /* Default node loader service
-         */
-        "node-loader" : {
-            loadNode: function(nodeId) {
-            }
-        }
-    };
+    this._services = {};
 
     this.addService = function(name, service) {
         this._services[name] = service;
@@ -24,34 +15,17 @@ SceneJS.Services = new (function() {
     this.getService = function(name) {
         return this._services[name];
     };
+
+    /*----------------------------------------------------
+     * Install default services
+     *---------------------------------------------------*/
+
+    this.addService(this.NODE_LOADER_SERVICE_ID, {
+        loadNode: function(nodeId) {
+        }
+    });
+
+    /* Namespace for core implementations    
+     */
+    this.impl = {};
 })();
-
-SceneJS.Services.impl = {
-
-    NodeFileLoader : function (cfg) {
-
-        this._loads = {};
-
-        /**
-         * Loads the node of the given ID if it does not exist and is not already loading
-         */
-        this.loadNode = function(nodeId) {
-            if (!SceneJS.nodeExists(nodeId) && this._loads[nodeId]) {
-                this._loadNode(nodeId);
-            }
-        };
-
-        /** Performs the JSONP load.
-         */
-        this._loadNode = function(nodeId) {
-            var head = document.getElementsByTagName("head")[0];
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            head.appendChild(script);
-            this._loads[nodeId] = {
-                timeStarted: (new Date()).getTime()
-            };
-            script.src = cfg.urlStrategy(nodeId);
-        };
-    }
-};
