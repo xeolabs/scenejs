999372624c2918b2a344a2b32fc3770b5ad83548
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2010-10-12 14:45:41 +1300

    Stub for non-core scene utility to auto-create LOD

---
+++ src/utils/optimize/create-lod.js
@@ -0,0 +1,26 @@
+if (!SceneJS.utils) {
+    SceneJS.utils = {};
+}
+
+if (!SceneJS.utils.optimize) {
+    SceneJS.utils.optimize = {};
+}
+
+/**
+ * @class Utility which automatically creates LOD versions for geometry nodes in scene subgraphs
+ *  @constructor
+ * Create a new SceneJS.utils.optimize.CreateLOD
+ * @param {Object} cfg Configuration for this utility
+ */
+SceneJS.utils.optimize.CreateLOD = function(cfg) {
+    this._cfg = cfg;
+};
+
+SceneJS.utils.optimize.CreateLOD.execute = function(params, callback) {
+    if (!params.nodeId) {
+        throw "SceneJS.utils.optimize.CreateLOD.execute expects params.nodeId";
+    }
+    if (!callback) {
+        throw "SceneJS.utils.optimize.CreateLOD.execute expects callback";
+    }
+};
