3e7450ac76f540739a6ec4a79fd38339efc93a3e
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2010-10-31 21:57:26 +1300

    Renderer node state now popping correctly

--- src/scenejs/renderer/renderer.js
+++ src/scenejs/renderer/renderer.js
@@ -6,14 +6,52 @@
  */
 SceneJS.Renderer = SceneJS.createNodeType("renderer");
 
+
+// @private
+SceneJS.Renderer.prototype._init = function(params) {
+    this._attr = {};
+    if (params.blend) {
+        this.setBlend(params.blend);
+    }
+    if (params.wireframe) {
+        this.setBlend(params.wireframe);
+    }
+    if (params.highlight) {
+        this.setBlend(params.highlight);
+    }
+};
+
+SceneJS.Renderer.prototype.setBlend = function(blend) {
+    this._props.blend = blend;
+};
+
+SceneJS.Renderer.prototype.getBlend = function() {
+    return this._props.blend;
+};
+
+SceneJS.Renderer.prototype.setWireframe = function(wireframe) {
+    this._props.wireframe = wireframe;
+};
+
+SceneJS.Renderer.prototype.getWireframe = function() {
+    return this.props.wireframe;
+};
+
+SceneJS.Renderer.prototype.setHighlight = function(highlight) {
+    this._props.highlight = highlight;
+};
+
+SceneJS.Renderer.prototype.getHighlight = function() {
+    return this.props.highlight;
+};
+
 // @private
 SceneJS.Renderer.prototype._render = function(traversalContext) {
     if (this._memoLevel == 0) {
-
         this._props = SceneJS._rendererModule.createProps(this._getParams());
         this._memoLevel = 1;
     }
     SceneJS._rendererModule.pushProps(this._props);
     this._renderNodes(traversalContext);
-    SceneJS._rendererModule.pushProps(this._props);
+    SceneJS._rendererModule.popProps(this._props);
 };
