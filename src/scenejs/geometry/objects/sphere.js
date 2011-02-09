725feca1d0154a5b965007e69b797bdfd500c7c0
Stephen Bannasch <stephen.bannasch@gmail.com>
2011-02-09 11:10:30 -0500

    optional sliceDepth attribute for SceneJS.sphere
    Sets depth percentage of slices to generate from top 
    to bottom (1 is default).
    
    Example:
    
    {
        type: "sphere",
        slices: 48,
        rings: 48,
        radius: 4,
        sliceDepth: 0.25,
    }

--- src/scenejs/geometry/objects/sphere.js
+++ src/scenejs/geometry/objects/sphere.js
@@ -9,6 +9,8 @@
  *          rings: 30,           // Optional number of latitudinal slices (30 is default)
  *          semiMajorAxis: 1.5,  // Optional semiMajorAxis results in elliptical sphere (default of 1 creates sphere)
  *          sweep: 0.75,         // Optional rotational extrusion (1 is default)
+ *          sliceDepth: 0.25,    // Optional depth of slices to generate from top to bottom (1 is default)
+ (1 is default)
  *     })
  * </pre></code>
 * @extends SceneJS.Geometry
@@ -19,7 +21,8 @@
  * @param {float} [cfg.slices=30] Number of longitudinal slices
  * @param {float} [cfg.rings=30] Number of longitudinal slices
  * @param {float} [cfg.semiMajorAxis=1.0] values other than one generate an elliptical sphere
- * @param {float} [cfg.sweep=1]  rotational extrusion default is 1
+ * @param {float} [cfg.sweep=1]  rotational extrusion, default is 1
+ * @param {float} [cfg.sliceDepth=1]  depth of slices to generate, default is 1
  * @param {function(SceneJS.Data):Object} [fn] Dynamic configuration function
  * @param {...SceneJS.Node} [childNodes] Child nodes
  */
@@ -29,7 +32,7 @@ SceneJS.Sphere = SceneJS.createNodeType("sphere", "geometry");
 SceneJS.Sphere.prototype._init = function(params) {
     var slices = params.slices || 30;
     var rings = params.rings || 30;
-	var radius = params.radius || 1;
+    var radius = params.radius || 1;
     
     var semiMajorAxis =  params.semiMajorAxis || 1;
     var semiMinorAxis =  1 / semiMajorAxis;
@@ -38,14 +41,19 @@ SceneJS.Sphere.prototype._init = function(params) {
     if (sweep > 1) {
         sweep = 1
     }
-    
+
+    var sliceDepth = params.sliceDepth || 1;
+    if (sliceDepth > 1) {
+        sliceDepth = 1
+    }
+
     var ringLimit = rings * sweep;
-    
+    var sliceLimit = slices * sliceDepth;
 
     /* Resource ID ensures that we reuse any sphere that has already been created with
      * these parameters instead of wasting memory
      */
-    this._resource = "sphere_" + radius + "_" + rings + "_" + slices + "_" + semiMajorAxis + "_" + sweep;
+    this._resource = "sphere_" + radius + "_" + rings + "_" + slices + "_" + semiMajorAxis + "_" + sweep + "_" + sliceDepth;
 
     /* Callback that does the creation in case we can't find matching sphere to reuse     
      */
@@ -53,12 +61,15 @@ SceneJS.Sphere.prototype._init = function(params) {
         var positions = [];
         var normals = [];
         var uv = [];
-        for (var sliceNum = 0; sliceNum <= slices; sliceNum++) {
+        var ringNum, sliceNum, index;
+
+        for (sliceNum = 0; sliceNum <= slices; sliceNum++) {
+            if (sliceNum > sliceLimit) break;
             var theta = sliceNum * Math.PI / slices;
             var sinTheta = Math.sin(theta);
             var cosTheta = Math.cos(theta);
 
-            for (var ringNum = 0; ringNum <= rings; ringNum++) {
+            for (ringNum = 0; ringNum <= rings; ringNum++) {
                 if (ringNum > ringLimit) break;
                 var phi = ringNum * 2 * Math.PI / rings;
                 var sinPhi = semiMinorAxis * Math.sin(phi);
@@ -81,15 +92,27 @@ SceneJS.Sphere.prototype._init = function(params) {
             }
         }
 
+        // create a center point which is only used when sweep or sliceDepth are less than one
+        if (sliceDepth < 1) {
+            var yPos = Math.cos((sliceNum - 1) * Math.PI / slices) * radius;
+            positions.push(0, yPos, 0);
+            normals.push(1, 1, 1);
+            uv.push(1, 1);
+        } else {
+            positions.push(0, 0, 0);
+            normals.push(1, 1, 1);
+            uv.push(1, 1);
+        }
+
+        // index of the center position point in the positions array
+        // var centerIndex = (ringLimit + 1) *  (sliceLimit);
+        var centerIndex = positions.length / 3 - 1;
+        
         var indices = [];
         
-        positions.push(0, 0, 0);
-        normals.push(1, 1, 1);
-        uv.push(1, 1);
-        var center = (ringLimit + 1) *  (slices + 1);
-        
-        for (var sliceNum = 0; sliceNum < slices; sliceNum++) {
-            for (var ringNum = 0; ringNum < rings; ringNum++) {
+        for (sliceNum = 0; sliceNum < slices; sliceNum++) {
+            if (sliceNum >= sliceLimit) break;
+            for (ringNum = 0; ringNum < rings; ringNum++) {
                 if (ringNum >= ringLimit) break;
                 var first = (sliceNum * (ringLimit + 1)) + ringNum;
                 var second = first + ringLimit + 1;
@@ -106,11 +129,19 @@ SceneJS.Sphere.prototype._init = function(params) {
                 //  indices for a sphere with fours ring-segments when only two are drawn.
                 index = (ringLimit + 1) * sliceNum;
                 //    0,3,15   2,5,15  3,6,15  5,8,15 ...
-                indices.push(index, index + ringLimit + 1, center);
-                indices.push(index + ringLimit, index + ringLimit * 2 + 1, center);
+                indices.push(index, index + ringLimit + 1, centerIndex);
+                indices.push(index + ringLimit, index + ringLimit * 2 + 1, centerIndex);
              }
-            
         }
+
+        if (slices > sliceLimit) {
+            // We aren't sweeping from the top all the way to the bottom so ... 
+            for (ringNum = 1; ringNum <= ringLimit; ringNum++) {
+                index = sliceNum * ringLimit + ringNum;
+                indices.push(index, index + 1, centerIndex);
+            }
+            indices.push(index + 1, sliceNum * ringLimit + 1, centerIndex);
+         }
 
         return {
             primitive : "triangles",
