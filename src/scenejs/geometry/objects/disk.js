3a886a02cafbcfd051ee934ae71e9cbd96eb59ff
Stefan C. Mueller <scm@smurn.org>
2011-11-25 11:09:43 +0100

    Changed disk geometry creation to make disk nodes visible. Refactored the creation pattern to what is used by the other geometry nodes.

--- src/scenejs/geometry/objects/disk.js
+++ src/scenejs/geometry/objects/disk.js
@@ -52,202 +52,205 @@
 
         var ringLimit = rings * sweep;
 
-        /* Resource ID ensures that we reuse any sphere that has already been created with
-         * these parameters instead of wasting memory
-         */
-        this.coreId = "disk_" + radius + "_" + height + "_" + rings + "_" + innerRadius + "_" + semiMajorAxis + "_" + sweep;
+        SceneJS_geometry.prototype._init.call(this, {
 
-        /* Callback that does the creation in case we can't find matching disk to reuse
-         */
-        this._create = function() {
-            var positions = [];
-            var normals = [];
-            var uv = [];
+            /* Resource ID ensures that we reuse any sphere that has already been created with
+             * these parameters instead of wasting memory
+             */
+            coreId : params.coreId || "disk_" + radius + "_" + height + "_" + rings + "_" + innerRadius + "_" + semiMajorAxis + "_" + sweep,
 
-            var ybot = height * -0.5;
-            var ytop = height * 0.5;
+            /* Callback that does the creation in case we can't find matching disk to reuse
+             */
+            create : function() {
+                var positions = [];
+                var normals = [];
+                var uv = [];
 
-            for (var ringNum = 0; ringNum <= rings; ringNum++) {
-                if (ringNum > ringLimit) break;
-                var phi = ringNum * 2 * Math.PI / rings;
-                var sinPhi = semiMinorAxis * Math.sin(phi);
-                var cosPhi = semiMajorAxis * Math.cos(phi);
+                var ybot = height * -0.5;
+                var ytop = height * 0.5;
 
-                var x = cosPhi;
-                var z = sinPhi;
-                var u = 1 - (ringNum / rings);
-                var v = 0.5;
+                for (var ringNum = 0; ringNum <= rings; ringNum++) {
+                    if (ringNum > ringLimit) break;
+                    var phi = ringNum * 2 * Math.PI / rings;
+                    var sinPhi = semiMinorAxis * Math.sin(phi);
+                    var cosPhi = semiMajorAxis * Math.cos(phi);
+
+                    var x = cosPhi;
+                    var z = sinPhi;
+                    var u = 1 - (ringNum / rings);
+                    var v = 0.5;
+
+                    //
+                    // create the outer set of vertices,
+                    // one for the bottom and one for the top
+                    //
+                    normals.push(-x);
+                    normals.push(1);
+                    normals.push(-z);
+                    uv.push(u);
+                    uv.push(v);
+                    positions.push(radius * x);
+                    positions.push(ybot);
+                    positions.push(radius * z);
+
+                    normals.push(-x);
+                    normals.push(-1);
+                    normals.push(-z);
+                    uv.push(u);
+                    uv.push(v);
+                    positions.push(radius * x);
+                    positions.push(ytop);
+                    positions.push(radius * z);
+
+                    if (innerRadius > 0) {
+                        //
+                        // Creating a disk with a hole in the middle ...
+                        // generate the inner set of vertices,
+                        // one for the bottom and one for the top
+                        //
+                        normals.push(-x);
+                        normals.push(-ytop);
+                        normals.push(-z);
+                        uv.push(u);
+                        uv.push(v);
+                        positions.push(innerRadius * x);
+                        positions.push(ytop);
+                        positions.push(innerRadius * z);
+
+                        normals.push(-x);
+                        normals.push(-ybot);
+                        normals.push(-z);
+                        uv.push(u);
+                        uv.push(v);
+                        positions.push(innerRadius * x);
+                        positions.push(ybot);
+                        positions.push(innerRadius * z);
+                    }
+
+                }
+
+                var indices = [];
 
                 //
-                // create the outer set of vertices,
-                // one for the bottom and one for the top
+                // Create indices pointing to vertices for the top, bottom
+                // and optional endcaps for the disk
                 //
-                normals.push(-x);
-                normals.push(1);
-                normals.push(-z);
-                uv.push(u);
-                uv.push(v);
-                positions.push(radius * x);
-                positions.push(ybot);
-                positions.push(radius * z);
-
-                normals.push(-x);
-                normals.push(-1);
-                normals.push(-z);
-                uv.push(u);
-                uv.push(v);
-                positions.push(radius * x);
-                positions.push(ytop);
-                positions.push(radius * z);
 
                 if (innerRadius > 0) {
                     //
                     // Creating a disk with a hole in the middle ...
-                    // generate the inner set of vertices,
-                    // one for the bottom and one for the top
+                    // Each ring sengment rquires a quad surface on the top and bottom
+                    // so create two traingles for the top and two for the bottom.
                     //
-                    normals.push(-x);
-                    normals.push(-ytop);
-                    normals.push(-z);
-                    uv.push(u);
-                    uv.push(v);
-                    positions.push(innerRadius * x);
-                    positions.push(ytop);
-                    positions.push(innerRadius * z);
+                    var index;
+                    for (var ringNum = 0; ringNum < rings; ringNum++) {
+                        if (ringNum >= ringLimit) {
+                            // We aren't sweeping the whole way around so also create triangles to cap the ends.
+                            // Example: indices for a disk with fours ring-segments when only two are drawn.
+                            //   0,1,2   0,2,3  8,9,10  8,10,11
+                            indices.push(0, 1, 2);
+                            indices.push(0, 2, 3);
+                            index = ringLimit * 4;
+                            indices.push(index, index + 1, index + 2);
+                            indices.push(index, index + 2, index + 3);
+                            break;
+                        }
+                        index = ringNum * 4;
 
-                    normals.push(-x);
-                    normals.push(-ybot);
-                    normals.push(-z);
-                    uv.push(u);
-                    uv.push(v);
-                    positions.push(innerRadius * x);
-                    positions.push(ybot);
-                    positions.push(innerRadius * z);
-                }
+                        indices.push(index + 0);
+                        indices.push(index + 1);
+                        indices.push(index + 4);
 
-            }
+                        indices.push(index + 1);
+                        indices.push(index + 4);
+                        indices.push(index + 5);
 
-            var indices = [];
+                        indices.push(index + 1);
+                        indices.push(index + 2);
+                        indices.push(index + 5);
 
-            //
-            // Create indices pointing to vertices for the top, bottom
-            // and optional endcaps for the disk
-            //
+                        indices.push(index + 2);
+                        indices.push(index + 5);
+                        indices.push(index + 6);
 
-            if (innerRadius > 0) {
-                //
-                // Creating a disk with a hole in the middle ...
-                // Each ring sengment rquires a quad surface on the top and bottom
-                // so create two traingles for the top and two for the bottom.
-                //
-                var index;
-                for (var ringNum = 0; ringNum < rings; ringNum++) {
-                    if (ringNum >= ringLimit) {
-                        // We aren't sweeping the whole way around so also create triangles to cap the ends.
+                        indices.push(index + 2);
+                        indices.push(index + 3);
+                        indices.push(index + 6);
+
+                        indices.push(index + 3);
+                        indices.push(index + 6);
+                        indices.push(index + 7);
+
+                        indices.push(index + 3);
+                        indices.push(index + 0);
+                        indices.push(index + 7);
+
+                        indices.push(index + 0);
+                        indices.push(index + 7);
+                        indices.push(index + 4);
+                    }
+
+                } else {
+                    //
+                    // Create a solid disk without a hole in the middle ...
+                    // So only a single top and bottom triangle are needed
+                    //
+                    normals.push(0, 1.0, 0);
+                    uv.push(-1, -1);
+                    positions.push(0, ybot, 0);
+                    var centerBot = ringLimit * 2 + 2;
+
+                    normals.push(0, -1.0, 0);
+                    uv.push(-1, -1);
+                    positions.push(0, ytop, 0);
+                    var centerTop = ringLimit * 2 + 3;
+
+                    for (var ringNum = 0; ringNum < rings; ringNum++) {
+                        //  generate the two outer-facing triangles for each ring segment
+                        if (ringNum >= ringLimit) break;
+                        var index = ringNum * 2;
+                        indices.push(index);
+                        indices.push(index + 1);
+                        indices.push(index + 2);
+
+                        indices.push(index + 1);
+                        indices.push(index + 2);
+                        indices.push(index + 3);
+                    }
+
+                    for (var ringNum = 0; ringNum < rings; ringNum++) {
+                        //  generate the top and bottom triabgkle for each ring segment
+                        if (ringNum >= ringLimit) break;
+                        var index = ringNum * 2;
+                        indices.push(index);
+                        indices.push(index + 2);
+                        indices.push(centerBot);
+
+                        indices.push(index + 1);
+                        indices.push(index + 3);
+                        indices.push(centerTop);
+                    }
+
+                    if (rings >= ringLimit) {
+                        // We aren't sweeping the whole way around so create triangles to cap the ends.
                         // Example: indices for a disk with fours ring-segments when only two are drawn.
                         //   0,1,2   0,2,3  8,9,10  8,10,11
-                        indices.push(0, 1, 2);
-                        indices.push(0, 2, 3);
-                        index = ringLimit * 4;
-                        indices.push(index, index + 1, index + 2);
-                        indices.push(index, index + 2, index + 3);
-                        break;
+                        index = ringLimit * 2;
+                        indices.push(0, 1, index);
+                        indices.push(1, index + 1, index);
                     }
-                    index = ringNum * 4;
 
-                    indices.push(index + 0);
-                    indices.push(index + 1);
-                    indices.push(index + 4);
-
-                    indices.push(index + 1);
-                    indices.push(index + 4);
-                    indices.push(index + 5);
-
-                    indices.push(index + 1);
-                    indices.push(index + 2);
-                    indices.push(index + 5);
-
-                    indices.push(index + 2);
-                    indices.push(index + 5);
-                    indices.push(index + 6);
-
-                    indices.push(index + 2);
-                    indices.push(index + 3);
-                    indices.push(index + 6);
-
-                    indices.push(index + 3);
-                    indices.push(index + 6);
-                    indices.push(index + 7);
-
-                    indices.push(index + 3);
-                    indices.push(index + 0);
-                    indices.push(index + 7);
-
-                    indices.push(index + 0);
-                    indices.push(index + 7);
-                    indices.push(index + 4);
                 }
-
-            } else {
-                //
-                // Create a solid disk without a hole in the middle ...
-                // So only a single top and bottom triangle are needed
-                //
-                normals.push(0, 1.0, 0);
-                uv.push(-1, -1);
-                positions.push(0, ybot, 0);
-                var centerBot = ringLimit * 2 + 2;
-
-                normals.push(0, -1.0, 0);
-                uv.push(-1, -1);
-                positions.push(0, ytop, 0);
-                var centerTop = ringLimit * 2 + 3;
-
-                for (var ringNum = 0; ringNum < rings; ringNum++) {
-                    //  generate the two outer-facing triangles for each ring segment
-                    if (ringNum >= ringLimit) break;
-                    var index = ringNum * 2;
-                    indices.push(index);
-                    indices.push(index + 1);
-                    indices.push(index + 2);
-
-                    indices.push(index + 1);
-                    indices.push(index + 2);
-                    indices.push(index + 3);
-                }
-
-                for (var ringNum = 0; ringNum < rings; ringNum++) {
-                    //  generate the top and bottom triabgkle for each ring segment
-                    if (ringNum >= ringLimit) break;
-                    var index = ringNum * 2;
-                    indices.push(index);
-                    indices.push(index + 2);
-                    indices.push(centerBot);
-
-                    indices.push(index + 1);
-                    indices.push(index + 3);
-                    indices.push(centerTop);
-                }
-
-                if (rings >= ringLimit) {
-                    // We aren't sweeping the whole way around so create triangles to cap the ends.
-                    // Example: indices for a disk with fours ring-segments when only two are drawn.
-                    //   0,1,2   0,2,3  8,9,10  8,10,11
-                    index = ringLimit * 2;
-                    indices.push(0, 1, index);
-                    indices.push(1, index + 1, index);
-                }
-
+                
+                return {
+                    primitive : "triangles",
+                    positions : positions,
+                    normals: normals,
+                    uv : uv,
+                    indices : indices
+                };
             }
-            
-            return {
-                primitive : "triangles",
-                positions : positions,
-                normals: normals,
-                uv : uv,
-                indices : indices
-            };
-        };
+        });
     };
 
 })();
