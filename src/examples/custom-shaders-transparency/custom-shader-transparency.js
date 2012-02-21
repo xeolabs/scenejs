6675267fdd1479b743d75ade2769b259ca179ec3
Sascha Hendel <archinf@archinform.de>
2012-02-21 13:47:43 +0100

    Update src/examples/custom-shaders-transparency/custom-shader-transparency.js

--- src/examples/custom-shaders-transparency/custom-shader-transparency.js
+++ src/examples/custom-shaders-transparency/custom-shader-transparency.js
@@ -288,16 +288,38 @@ function mouseDown(event) {
     dragging = true;
 }
 
+function touchStart(event) {
+    lastX = event.targetTouches[0].clientX;
+    lastY = event.targetTouches[0].clientY;
+    dragging = true;
+}
+
 function mouseUp() {
     dragging = false;
 }
 
+function touchEnd() {
+    dragging = false;
+}
+
 function mouseMove(event) {
+    var posX = event.clientX;
+    var posY = event.clientY;
+    actionMove(posX,posY);
+}
+
+function touchMove(event) {
+    var posX = event.targetTouches[0].clientX;
+    var posY = event.targetTouches[0].clientY;
+    actionMove(posX,posY);
+}
+
+function actionMove(posX, posY) {
     if (dragging) {
-        yaw += (event.clientX - lastX) * 0.5;
-        pitch += (event.clientY - lastY) * 0.5;
-        lastX = event.clientX;
-        lastY = event.clientY;
+        yaw += (posX - lastX) * 0.5;
+        pitch += (posY - lastY) * 0.5;
+        lastX = posX;
+        lastY = posY;
 
         pitchRotate.set("angle", pitch);
         yawRotate.set("angle", yaw);
@@ -307,6 +329,9 @@ function mouseMove(event) {
 canvas.addEventListener('mousedown', mouseDown, true);
 canvas.addEventListener('mousemove', mouseMove, true);
 canvas.addEventListener('mouseup', mouseUp, true);
+canvas.addEventListener('touchstart', touchStart, true);
+canvas.addEventListener('touchmove', touchMove, true);
+canvas.addEventListener('touchend', touchEnd, true);
 
 scene.start();
 
