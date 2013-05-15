3a6224842ed6bc1470c7c3c5b1ce0734a176c4dc
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-05-15 09:06:44 +0200

    Added diffuse flag

---
+++ src/distWiring.js
@@ -0,0 +1,2 @@
+//SceneJS.configure({ pluginPath: "http://xeolabs.github.com/scenejs/build/latest/plugins" });
+SceneJS.configure({ pluginPath: "/home/lindsay/xeolabs/projects/scenejs3.0/build/latest/plugins" });
