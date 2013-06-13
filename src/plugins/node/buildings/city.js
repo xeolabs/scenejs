95d8e86f0f4bf8b37fe441a1c096fcaf8b6b493d
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-06-13 02:44:07 +0200

    City width option

--- src/plugins/node/buildings/city.js
+++ src/plugins/node/buildings/city.js
@@ -2,12 +2,18 @@ SceneJS.Types.addType("buildings/city", {
 
     init:function (params) {
 
+        var width = params.width || 600;
+        if (width < 100) {
+            throw "city width must be at least 100";
+        }
+        var halfWidth = width / 2;
+
         // Add a grid of buildings
-        for (var x = -300; x < 300; x += 50) {
-            for (var z = -300; z < 300; z += 50) {
+        for (var x = -halfWidth; x < halfWidth; x += 50) {
+            for (var z = -halfWidth; z < halfWidth; z += 50) {
                 this.addNode({
                     type:"buildings/building",
-                    buildingType: Math.floor(Math.random() * 3), // Three building types
+                    buildingType:Math.floor(Math.random() * 3), // Three building types
                     pos:{
                         x:x,
                         y:0,
