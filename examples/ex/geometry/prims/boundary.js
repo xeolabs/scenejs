c39b64e246592d4da2da07b5db8d5e4ef3be3eb5
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-06-15 23:27:39 +0200

    Camera orbit for boundary example

--- examples/ex/geometry/prims/boundary.js
+++ examples/ex/geometry/prims/boundary.js
@@ -1,23 +1,28 @@
 var scene = SceneJS.createScene({
     nodes:[
         {
-            type:"material",
-            color:{ r:0.6, g:0.6, b:0.9 },
+            type:"camera/orbit",
+            yaw:40,
+            pitch:-20,
+            zoom:10,
+            zoomSensitivity:10.0,
+            eye:{ x:0, y:0, z:10 },
+            look:{ x:0, y:0, z:0 },
+
             nodes:[
                 {
-                    type:"geometry",
-                    source:{
-                        type:"box"
-                    }
+                    type:"material",
+                    color:{ r:0.6, g:0.6, b:0.9 },
+                    nodes:[
+                        {
+                            type:"geometry",
+                            source:{
+                                type:"box"
+                            }
+                        }
+                    ]
                 }
             ]
         }
     ]
 });
-
-new SceneJS.OrbitControls(scene, {
-    yaw:30,
-    pitch:-30,
-    zoom:5
-});
-
