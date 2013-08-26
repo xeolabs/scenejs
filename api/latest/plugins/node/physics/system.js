3a7deba9f015c5e29a4e38544a990edbc36c608f
Lindsay Kay <lindsay.stanley.kay@gmail.com>
2013-08-26 03:14:46 +0200

    Small tidy up

--- api/latest/plugins/node/physics/system.js
+++ api/latest/plugins/node/physics/system.js
@@ -17,15 +17,7 @@ require([
                 // When params.systemId is undefined, get the default system for this scene
                 this._system = physics.getSystem(this.getScene(), params.systemId);
 
-                try {
-
-                    // There must be no overlap in params for SceneJS and the physics system
-                    // because we are just passing those params straight through to the system
-
-                    this.setConfigs(params);
-                } catch (e) {
-                    console.log("Failed to create 'physics/system' node ID '" + this.getId() + "' : " + e);
-                }
+                this.setConfigs(params);
             },
 
             /**
