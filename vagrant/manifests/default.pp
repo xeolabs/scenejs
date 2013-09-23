1bb7b3f4cbd80ff96643e45a9209466964f0f214
Lucas Doyle <lukedoyle@mujin.co.jp>
2013-09-23 17:30:12 +0900

    add vagrant vm because i can't run jekyll natively

---
+++ vagrant/manifests/default.pp
@@ -0,0 +1,37 @@
+class must-have {
+  include apt
+
+  apt::ppa { "ppa:chris-lea/node.js": }
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update',
+    before => Apt::Ppa["ppa:chris-lea/node.js"],
+  }
+
+  exec { 'apt-get update 2':
+    command => '/usr/bin/apt-get update',
+    require => Apt::Ppa["ppa:chris-lea/node.js"],
+  }
+
+  exec { 'install gem jekyll':
+    command => '/opt/ruby/bin/gem install jekyll',
+    creates => "/opt/ruby/bin/jekyll",
+    require => Exec["apt-get update 2"],
+  }
+
+  exec { 'install npm deps':
+    command => '/usr/bin/npm install -g grunt-cli bower',
+    creates => [
+      '/usr/lib/node_modules/bower/bin/bower',
+      '/usr/lib/node_modules/grunt-cli/bin/grunt',
+    ],
+    require => [ Exec["apt-get update 2"], Package["nodejs"] ],
+  }
+
+  package {["build-essential", "bash", "nodejs", "git-core"]:
+    ensure => present,
+    require => Exec["apt-get update 2"],
+  }
+}
+
+include must-have
