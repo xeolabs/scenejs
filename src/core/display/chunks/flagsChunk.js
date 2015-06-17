03efa0fc998ca9dae286ba4b541e92b98189f407
xeolabs <lindsay.kay@xeolabs.com>
2015-06-17 23:10:32 +0200

    Change transparency blend function  https://github.com/xeolabs/scenejs/issues/368

--- src/core/display/chunks/flagsChunk.js
+++ src/core/display/chunks/flagsChunk.js
@@ -52,7 +52,7 @@ SceneJS_ChunkFactory.createChunkType({
                     // Entering a transparency bin
 
                     gl.enable(gl.BLEND);
-                    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+                    gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
                     frameCtx.blendEnabled = true;
 
                 } else {
