9b60a048b824ac7578881b363c3867986736f243
Tarek Sherif <tsherif@gmail.com>
2015-10-07 18:01:43 -0400

    Fixed hashing and precision of region maps

--- src/core/display/programSourceFactory.js
+++ src/core/display/programSourceFactory.js
@@ -1042,7 +1042,7 @@ var SceneJS_ProgramSourceFactory = new (function () {
             // Region map highlighting
 
             src.push("vec3 regionColor = texture2D(SCENEJS_uRegionMapSampler, vec2(SCENEJS_vRegionMapUV.s, 1.0 - SCENEJS_vRegionMapUV.t)).rgb;");
-            src.push("float tolerance = 0.03;");
+            src.push("float tolerance = 0.01;");
             src.push("float colorDelta = dot(abs(SCENEJS_uRegionMapHighlightColor - regionColor), vec3(1.0));");
             src.push("if (colorDelta < tolerance) {");
             src.push("  fragColor.rgb *= SCENEJS_uRegionMapHighlightFactor;");
