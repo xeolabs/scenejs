f2bb79566657395e62649ca1239ed391607134b9
Tarek Sherif <tsherif@gmail.com>
2016-01-07 16:41:44 -0500

    Fix bug in modelXFormStack.pop()

--- src/core/scene/modelXFormStack.js
+++ src/core/scene/modelXFormStack.js
@@ -208,10 +208,8 @@ var SceneJS_modelXFormStack = new (function () {
     this.pop = function () {
 
         this.top = (--stackLen > 0) ? transformStack[stackLen - 1] : defaultCore;
-        if (this.top) {
-            this.top.cores.pop();
-            this.top.numCores--;
-        }
+        transformStack.length = stackLen;  // Release previous top node
+
         dirty = true;
     };
 
