1c6d8673ec235d3661ce9a960299e0479ca8f5bb
xeolabs <lindsay.kay@xeolabs.com>
2016-03-09 15:03:18 +0100

    Snapshot build

--- api/latest/plugins/node/models/backgrounds/water.js
+++ api/latest/plugins/node/models/backgrounds/water.js
@@ -36,11 +36,11 @@ SceneJS.Types.addType("models/backgrounds/water", {
                     stage: "vertex",
                     code: [
                         "attribute vec3 SCENEJS_aVertex;",
-                        "attribute vec2 SCENEJS_aUVCoord;",
+                        "attribute vec2 SCENEJS_aUVCoord0;",
                         "varying vec2 surfacePosition;",
                         "void main () {",
                         "    gl_Position = vec4(SCENEJS_aVertex.x, SCENEJS_aVertex.y, 0.98, 1.0);",
-                        "    surfacePosition = SCENEJS_aUVCoord;",
+                        "    surfacePosition = SCENEJS_aUVCoord0;",
                         "}"
                     ]
                 },
