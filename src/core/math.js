5bf05a387945a0b1a0d6a068fa31870a051d4c77
xeolabs <lindsay.kay@xeolabs.com>
2016-08-26 13:34:21 +0200

    Added math function: rotationTranslationMat4

--- src/core/math.js
+++ src/core/math.js
@@ -1267,6 +1267,56 @@
     };
 
     /**
+     * Creates a matrix from a quaternion rotation and vector translation
+     *
+     * @param {Float32Array} q Rotation quaternion
+     * @param {Float32Array} v Translation vector
+     * @param {Float32Array} dest Destination matrix
+     * @returns {Float32Array} dest
+     */
+    window.SceneJS_math_rotationTranslationMat4 = function (q, v, dest) {
+
+        dest = dest || SceneJS_math_mat4();
+
+        var x = q[0];
+        var y = q[1];
+        var z = q[2];
+        var w = q[3];
+
+        var x2 = x + x;
+        var y2 = y + y;
+        var z2 = z + z;
+        var xx = x * x2;
+        var xy = x * y2;
+        var xz = x * z2;
+        var yy = y * y2;
+        var yz = y * z2;
+        var zz = z * z2;
+        var wx = w * x2;
+        var wy = w * y2;
+        var wz = w * z2;
+
+        dest[0] = 1 - (yy + zz);
+        dest[1] = xy + wz;
+        dest[2] = xz - wy;
+        dest[3] = 0;
+        dest[4] = xy - wz;
+        dest[5] = 1 - (xx + zz);
+        dest[6] = yz + wx;
+        dest[7] = 0;
+        dest[8] = xz + wy;
+        dest[9] = yz - wx;
+        dest[10] = 1 - (xx + yy);
+        dest[11] = 0;
+        dest[12] = v[0];
+        dest[13] = v[1];
+        dest[14] = v[2];
+        dest[15] = 1;
+
+        return dest;
+    };
+
+    /**
      * Default lookat properties - eye at 0,0,1, looking at 0,0,0, up vector pointing up Y-axis
      */
     window.SceneJS_math_LOOKAT_OBJ = {
